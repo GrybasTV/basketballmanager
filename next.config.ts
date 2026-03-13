4b6a163996749468ef3a72127995bcd36d297453
GrybasTV <grybastv@example.com>
2026-03-13 22:15:20 +0200

    Update next.config and fix auth setup for Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["next-auth"],
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
