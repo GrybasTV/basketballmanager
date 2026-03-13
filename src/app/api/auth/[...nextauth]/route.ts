aa63be4e08eb43e8d5a6003c5f5791c8bf8445c5
GrybasTV <grybastv@example.com>
2026-03-13 22:05:32 +0200

    Enable edge runtime for all routes to support Cloudflare

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import NextAuth from "next-auth/next"
 import { authOptions } from "@/lib/auth"
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import Link from 'next/link';
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/lib/auth"
